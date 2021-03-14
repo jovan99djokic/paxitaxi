d135f3089066acde78fbc38c598737f1449f65db
jovan99djokic <jovan99djokicpn@gmail.com>
2021-03-14 04:16:48 +0100

    dodat je cenovnik

--- src/Elements/Header.js
+++ src/Elements/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './Header.css';
+import { useHistory } from 'react-router-dom';
 import Logo from '../Images/Logo.png';
 
 const Header = () => {
 
+    const history = useHistory();
+    const handleClick = (path) => history.push(path);
 
 
     return (
         <div className="header">
-            <button className="button">Cenovnik</button>
-            <img src={Logo} alt="Logo"></img>
+            <button className="button" onClick={() => handleClick('cenovnik')}>Cenovnik</button>
+            <h1 className="h1">063-568-666</h1>
+            <img src={Logo} alt="Logo" onClick={() => handleClick('/')}></img>
             
         </div>
 
--- src/Pages/Cenovnik.js
+++ src/Pages/Cenovnik.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Header from '../Elements/Header'
 
+import './Cenovnik.css'
+
+import Prvi_odradjen from '../Images/Prvi_odradjen.jpg'
+import Drugi_odradjen from '../Images/Drugi_odradjen.jpg'
+
 function Cenovnik() {
     return (
         <div>
             <Header></Header>
-            <h1>Cenovnik</h1>
+            <img className="img" src={Prvi_odradjen} alt="Cenovnik"></img>
+            <img className="img" src={Drugi_odradjen} alt="Cenovnik"></img>
         </div>
     )
 }
