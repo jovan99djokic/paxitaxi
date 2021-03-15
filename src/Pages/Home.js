a36e98b2b31c52940bccf7dffed8313078dfa2af
jovan99djokic <jovan99djokicpn@gmail.com>
2021-03-15 03:19:37 +0100

    Uradjena pocetna skroz

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Header from '../Elements/Header';
 import './Home.css';
+import './Razgledanje.css'
+import './mek.css'
 import Pozadina from '../Images/Pozadina.jpg';
 import Aerodrom from '../Images/aerodrom.jpg';
+import razgledanje from '../Images/razgledanje.jpg'
+import mek from '../Images/mek.jpg'
 
 const Home = () => {
     return (
@@ -22,6 +26,22 @@ const Home = () => {
                 <img src={Aerodrom} alt="Aerodrom-Slika"></img>
         </div>
 
+        <div className="tekst">
+                <h1>Turištičko razgledanje opštine Paraćin, obilazak svih verskih i kulturnih znamenosti.</h1>
+                <h2>Paxi Taxi u svojoj ponudi ima i usluge prevoza izvan granica Srbije.Komgornim i modernim automobilom i profesionalnim vozačem.Vaše putovanje biće sigurno i udobno.Za više informacija o vangraničnom prevozu kontaktirajte nas putem broja telefona ili mejla.</h2>
+        </div>
+
+        <div className="slika">
+                <img src={razgledanje} alt="Turisticko razgledanje grada"></img>
+        </div>
+
+        <div className="tekst1">
+                <h1>Paxi Taxi u svojoj ponudi ima i usluge prevoza izvan granica Srbije.Komgornim i modernim automobilom i profesionalnim vozačem.Vaše putovanje biće sigurno i udobno.Za više informacija o vangraničnom prevozu kontaktirajte nas putem broja telefona ili mejla.</h1>
+        </div>
+
+        <div className="slika1">
+                <img src={mek} alt="Turisticko razgledanje grada"></img>
+        </div>
         </>
     )
 }
